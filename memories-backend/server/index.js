8d7ec1ba1831c3e41f124d4e5b735b81177013b7
Shubham <shubham.prasad21@gmail.com>
2021-10-20 02:17:13 +0530

    initial commit

---
+++ memories-backend/server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Memories API");
+})
+
+//MONGO STUFF
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch(() => console.log(error.message))
