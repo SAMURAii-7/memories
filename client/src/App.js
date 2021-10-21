d86c5e9520cde54fd64d90213a4a5cfbaf683804
Shubham <shubham.prasad21@gmail.com>
2021-10-21 23:18:19 +0530

    make navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import { getPosts } from "./actions/posts";
-import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Forms/Form";
 import useStyles from "./styles";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -19,10 +19,7 @@ const App = () => {
 
     return (
         <Container maxidth="lg" >
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
+            <Navbar />
             <Grow in >
                 <Container>
                     <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memories-api-samuraii.herokuapp.com/posts";
+const url = "http://localhost:5000/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
