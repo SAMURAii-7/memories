9823cbd62d9cb8d2f9e45a54a2538d8881f51c5f
Shubham <shubham.prasad21@gmail.com>
2022-06-04 11:14:48 +0530

    update dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { getPosts } from "./actions/posts";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Forms/Form";
-import useStyles from "./styles";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch])
-
     return (
-        <Container maxidth="lg" >
-            <Navbar />
-            <Grow in >
-                <Container>
-                    <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxidth="lg" >
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/auth" exact component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     );
 }
 
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons";
+
+import useStyles from "./styles";
+
+const Auth = () => {
+    const classes = useStyles();
+    const isSignup = false;
+    const handleSubmit = () => {
+
+    };
+    const handleChange = () => {
+
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <Paper className={classes.paper} elevation={3} >
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+                <form className={classes.form} onSubmit={handleSubmit} >
+                    <Grid container spacing={2}>
+                        {
+                            isSignup && (
+                                <>
+                                    <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
+                                    <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
+                                </>
+                            )
+                        }
+                    </Grid>
+                </form>
+            </Paper>
+        </Container>
+    )
+}
+
+export default Auth
