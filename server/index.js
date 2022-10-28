3c4bd46a3ae2d2e98b4a096c8859ef5fe6a21b88
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2022-10-28 21:40:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,4 +24,4 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch(() => console.log(error.message))
+    .catch((error) => console.log(error.message))
