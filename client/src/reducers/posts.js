53670e713942e6250ce3829c11cd2abb6aefd17b
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2022-10-28 22:02:15 +0530

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
 
-export default (posts = [], action) => {
+export default const posts = (posts = [], action) => {
     switch (action.type) {
         case DELETE:
             return posts.filter((post) => post._id !== action.payload);
